99563cab1ce4ae00cd04c145e0ea1242be78bd5c
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-04-16 22:09:39 -0600

    agregar cors

--- dist/server.js
+++ dist/server.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const signale_1 = require("signale");
-//kljkjkk
+const cors_1 = __importDefault(require("cors"));
 const HabitatRouter_1 = require("./habitat/infrastructure/HabitatRouter");
 const app = (0, express_1.default)();
 app.disable("x-powered-by");
 const signale = new signale_1.Signale();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use("/habitat", HabitatRouter_1.userRouter);
 app.listen(3004, () => {
     signale.success("Server online in port 3004");
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { Signale } from "signale";
+import cors from "cors"
 
-//kljkjkk
 
 import { userRouter } from "./habitat/infrastructure/HabitatRouter";
 
@@ -12,6 +12,7 @@ app.disable("x-powered-by");
 const signale = new Signale();
 
 app.use(express.json());
+app.use(cors())
 app.use("/habitat", userRouter);
 
 app.listen(3004, () => {
